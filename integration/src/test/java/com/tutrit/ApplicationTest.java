a6b1460a6d2b1664cf15e228824d93df8d351911
mikas <6666350@gmail.com>
2023-02-01 21:23:54 +0100

    Optimizes caching spring context in tests

--- integration/src/test/java/com/tutrit/ApplicationTest.java
+++ integration/src/test/java/com/tutrit/ApplicationTest.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tutrtit.Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@AutoConfigureMockMvc
 @SpringBootTest(classes = Application.class)
 public class ApplicationTest {
 
